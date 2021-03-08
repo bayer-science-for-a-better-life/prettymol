de91cd951592a2b323932ebfdcce72fb547dfb49
Santi Villalba <sdvillal@gmail.com>
2021-03-08 18:04:06 +0100

    use manimgl by default

--- prettymol/config.py
+++ prettymol/config.py
@@ -17,7 +17,7 @@ class Config:
 
     @staticmethod
     def prefer_manimgl() -> bool:
-        preferred_manim = os.environ.get('PRETTYMOL_PREFERRED_MANIM', ManimImplementation.manimce.name).lower()
+        preferred_manim = os.environ.get('PRETTYMOL_PREFERRED_MANIM', ManimImplementation.manimgl.name).lower()
         possible_manims = [item.value for item in ManimImplementation]
         if preferred_manim not in possible_manims:
             raise ValueError(f'Unknown manim {preferred_manim}, must be one of {possible_manims}')
