9a189d31db445eada1825add125f4237f3f36968
Santi Villalba <sdvillal@gmail.com>
2021-03-08 10:35:23 +0100

    add default directories to config

--- prettymol/config.py
+++ prettymol/config.py
@@ -1,5 +1,6 @@
 import os
 from enum import Enum
+from pathlib import Path
 
 
 class ManimImplementation(Enum):
@@ -9,9 +10,14 @@ class ManimImplementation(Enum):
 
 class Config:
 
+    DEFAULT_MEDIA_PATH = Path(__file__).parent.parent / 'media'
+    DEFAULT_VIDEOS_PATH = DEFAULT_MEDIA_PATH / 'videos'
+    DEFAULT_RENDERED_SCENES_PATH = DEFAULT_VIDEOS_PATH
+    DEFAULT_IMAGES_PATH = DEFAULT_MEDIA_PATH / 'images'
+
     @staticmethod
     def prefer_manimgl() -> bool:
-        preferred_manim = os.environ.get('PRETTYMOL_PREFERRED_MANIM', ManimImplementation.manimgl.name).lower()
+        preferred_manim = os.environ.get('PRETTYMOL_PREFERRED_MANIM', ManimImplementation.manimce.name).lower()
         possible_manims = [item.value for item in ManimImplementation]
         if preferred_manim not in possible_manims:
             raise ValueError(f'Unknown manim {preferred_manim}, must be one of {possible_manims}')
